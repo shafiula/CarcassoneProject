e6acac00cda39e8a47388042b075ab4e53b1d4df
garywan <garywan@eagles.cse.buffalo.edu>
2015-09-30 17:23:21 -0400

    Changes to tile

--- CarcassonneProject/src/code/Tile.java
+++ CarcassonneProject/src/code/Tile.java
@@ -1,5 +1,25 @@
 package code;
 
-public class Tile {
+import java.util.ArrayList;
 
+public class Tile {
+	public ArrayList<Tile> tileSet = new ArrayList<Tile>();
+	public Tile(){
+	}
+	public void placeTile (){
+	}
+	public void drawTile(){
+		
+	}
+	public void removeTile(){
+		
+	}
+	public void rotateTile(){
+		
+	}
+	public void generateTiles(){
+	}
+	private boolean validPlacement(){
+		return true;
+	}
 }
