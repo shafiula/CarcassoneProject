f130452fd66a1e4b572d74f68e06d5b4e641fd2d
garywan <garywan@eagles.cse.buffalo.edu>
2015-09-30 17:57:40 -0400

    Tile update

--- CarcassonneProject/src/code/Tile.java
+++ CarcassonneProject/src/code/Tile.java
@@ -1,12 +1,20 @@
 package code;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Tile {
-	public ArrayList<Tile> tileSet = new ArrayList<Tile>();
-	public Tile(){
+	private HashMap<Integer, Integer> _side;
+	public Tile(City c, Field f, Road r){
+		_side = new HashMap<>();
+		_side // (0,x) = north; (1,x) = east; (2,x) = west; (3,x) = south
+		// (x, 0) = City; (x, 1) = Field, (x, 2) = Road
+		if(hasCity = true){
+			// which sides have city
+		}
 	}
 	public void placeTile (){
+		
 	}
 	public void drawTile(){
 		
@@ -19,7 +27,7 @@ public class Tile {
 	}
 	public void generateTiles(){
 	}
-	private boolean validPlacement(){
+	private boolean validPlacement(Tile t){
 		return true;
 	}
 }
